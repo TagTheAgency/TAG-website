bd2c06d0d9036eef6bc75804c3707ca6d0f10708
Unknown <charliematina@gmail.com>
2018-03-08 13:06:14 +1300

    Set Up Folder Structure
    Set up the folder structure and config.php. Added a normalize.css and added basic php templates. Created a SASS file for css as well as a minified version.

---
+++ public_html/index.php
@@ -0,0 +1,8 @@
+<?php
+	// Load up the config file
+	require_once("resources/config.php");
+
+	require_once(TEMPLATES_PATH . "/header.php");
+?>
+<?php require_once(TEMPLATES_PATH . "/nav.php"); ?>
+<?php require_once(TEMPLATES_PATH . "/footer.php"); ?>
---
+++ public_html/resources/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>TAG The Agency</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/style.min.css">
+	</head>
+	<body>
