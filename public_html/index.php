5b42dc472f7dd86657fed84ed39ec5aa2c63ccd0
Unknown <charliematina@gmail.com>
2018-03-08 14:37:53 +1300

    Added homepage elements
    Added homepage elements and layout divs + added code to the stylesheet.

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,38 @@
 <?php
 	// Load up the config file
 	require_once("resources/config.php");
-
 	require_once(TEMPLATES_PATH . "/header.php");
+	require_once(TEMPLATES_PATH . "/nav.php");
 ?>
-<?php require_once(TEMPLATES_PATH . "/nav.php"); ?>
+
+<div class="homepage-block landing">
+	<div>
+		<h3>Digital storyllers for brands</h3>
+	</div>
+</div>
+<div class="homepage-block strategy shadow">
+	<div>
+		<h2>Strategy</h2>
+		<p class="subheader">Stories start with a plan.</p>
+	</div>
+</div>
+<div class="homepage-block how shadow">
+	<div>
+		<h2>How</h2>
+		<p class="subheader">Stories start with a plan.</p>
+	</div>
+</div>
+<div class="homepage-block who shadow">
+	<div>
+		<h2>Who</h2>
+		<p class="subheader">Stories start with a plan.</p>
+	</div>
+</div>
+<div class="homepage-block get-in-touch shadow">
+	<div>
+		<h3>Get In Touch</h3>
+		<p class="subheader">Stories start with a plan.</p>
+	</div>
+</div>
+
 <?php require_once(TEMPLATES_PATH . "/footer.php"); ?>
--- public_html/resources/templates/header.php
+++ public_html/resources/templates/header.php
@@ -2,7 +2,10 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="TAG The Agency">
 		<title>TAG The Agency</title>
+		<link rel="stylesheet" href="https://use.typekit.net/lkt4neq.css">
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/style.min.css">
 	</head>
