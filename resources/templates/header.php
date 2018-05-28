e08b6f78c67a3d542a0e7c32e09efa0c4acbce5a
Colin Matcham <cmatcham@gmail.com>
2018-05-29 10:16:51 +1200

    SEO

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="TAG The Agency">
+		<meta name="description" content="Digital story tellers for brands">
 		<title>TAG The Agency</title>
 		<link rel="stylesheet" href="https://use.typekit.net/heh7zye.css">
 		<link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
